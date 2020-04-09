e2be86bdde0071c35c45363bcd88dd52ebfd31d2
Vic Cross <48090243+viccross@users.noreply.github.com>
2020-04-10 00:50:55 +1000

    Add RACF-mode password change

--- htdocs/resetpassword.php
+++ htdocs/resetpassword.php
@@ -36,11 +36,33 @@ if ($result === "") {
     $result = $ldap_connection[1];
 
     if ($ldap) {
-        $entry["userPassword"] = $password;
-	if ( $pwdreset === "true" ) {
-            $entry["pwdReset"] = "TRUE";
-	}
-	$modification = ldap_mod_replace($ldap, $dn, $entry);
+        if ( $racf_mode ) {
+            if ( $sdbm_base ) {
+                $result = ldap_explode_dn($dn, 0);
+                $data = ldap_search($ldap, $ldap_base, "($result[0])", array( "ibm-nativeId" ) );
+                $result = ldap_get_attributes($ldap, ldap_first_entry($ldap, $data) );
+                $racfid = $result["ibm-nativeId"][0];
+                $racfdn = "racfId=$racfid,profileType=user,$sdbm_base";
+                $entry["racfPassword"] = array();
+                $modification = ldap_mod_del($ldap, $racfdn, $entry);
+                $entry["racfPassword"] = $password;
+                if ( $pwdreset === "false" ) {
+                    $entry["racfAttributes"] = "NOEXPIRED";
+                }
+                $modification = ldap_mod_add($ldap, $racfdn, $entry);
+            } else {
+                $entry["userPassword"] = "";
+                $modification = ldap_mod_del($ldap, $dn, $entry);
+                $entry["userPassword"] = $password;
+                $modification = ldap_mod_add($ldap, $dn, $entry);
+            }
+        } else {
+            if ( $pwdreset === "true" ) {
+                $entry["pwdReset"] = "TRUE";
+            }
+            $entry["userPassword"] = $password;
+            $modification = ldap_mod_replace($ldap, $dn, $entry);
+        }
 	$errno = ldap_errno($ldap);
         if ( $errno ) {
             $result = "passwordrefused";
